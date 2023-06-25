980bda50e026a70b7d5534b5ab44e16d6fdb175e
Tam Le <letam210795@gmail.com>
2023-06-25 16:55:48 +0700

    Updated part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,23 @@
-const Total = () => {
-    const exercises1 = 10
-    const exercises2 = 7
-    const exercises3 = 14
+import {useState} from "react";
 
+
+const Total = ({parts}) => {
     return (
         <>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
+            <p>Number of exercises {parts.reduce((a, {exercises}) => a + exercises, 0)} </p>
         </>
     )
 }
-const Content = () => {
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
 
+const Content = ({parts}) => {
     return (
         <>
-        <Part name={part1} numberOfExercises={exercises1}/>
-        <Part name={part2} numberOfExercises={exercises2}/>
-        <Part name={part3} numberOfExercises={exercises3}/>
+            {
+                parts.map(part => <Part
+                    key={part.id}
+                    name={part.name}
+                    numberOfExercises={part.exercises}/>)
+            }
         </>
     )
 }
@@ -36,23 +32,110 @@ const Part = (props) => {
     )
 }
 
-const Header = (props) => {
+const Header = ({course}) => {
     return (
         <>
-            <h1>{props.course}</h1>
+            <h1>{course}</h1>
         </>
     )
 }
 
-const App = () => {
-    const course = 'Half Stack application development'
-
+const Display = ({counter}) => {
     return (
-        <div>
-            <Header course={course}/>
-            <Content/>
-            <Total/>
-        </div>
+        <div>{counter}</div>
+    )
+}
+
+const Button = ({handleClick, text}) => {
+    return (
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
+
+const History = ({allClicks}) => {
+    return (
+        allClicks.length === 0
+            ? <div>the app is used by pressing the buttons</div>
+            : <div>button press history: {allClicks.join(' ')}</div>
+    )
+}
+
+const App = (props) => {
+    const [counter, setCounter] = useState(0)
+
+    const increaseByOne = () => {
+        setCounter(counter + 1)
+    }
+
+    const decreaseByOne = () => {
+        setCounter(counter - 1)
+    }
+
+    const setToZero = () => {
+        setCounter(0)
+    }
+
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updatedLeft = left + 1
+        setLeft(updatedLeft)
+        setTotal(updatedLeft + right)
+    }
+
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        const updatedRight = right + 1
+        setRight(updatedRight)
+        setTotal(left + updatedRight)
+    }
+
+
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                id: "1",
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                id: "2",
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                id: "3",
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+            <Display counter={counter}/>
+            <Button handleClick={increaseByOne} text={"plus"}/>
+            <Button handleClick={setToZero} text={"zero"}/>
+            <Button handleClick={decreaseByOne} text={"minus"}/>
+            <div>
+                {left}
+                <Button handleClick={handleLeftClick} text={"left"}/>
+                <Button handleClick={handleRightClick} text={"right"}/>
+                {right}
+                <History allClicks={allClicks}/>
+                <p>total {total}</p>
+            </div>
+        </>
     )
 }
 
