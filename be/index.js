e9c3abad7dd5b01ea75a2c31f2ca080947eea1ce
Tam Le <letam210795@gmail.com>
2023-06-27 18:14:14 +0700

    Add eslint configuration

--- be/index.js
+++ be/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const app = express();
+const app = express()
 const Note = require('./models/note')
 
 app.use(cors())
@@ -32,7 +32,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
-        .then(result => {
+        .then(() => {
             response.status(204).end()
         })
         .catch(error => next(error))
